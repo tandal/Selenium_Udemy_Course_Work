d9f6f0a25eeffa5a90e806f1ad667c96a42fcd12
tylerandal <tyler.andal@gmail.com>
2021-06-26 07:47:20 -0500

    Class name selector lesson update.

---
+++ ClassName/EntryPointClassName.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPointClassName
+{
+    static void Main(string[] args)
+    {
+        string url = "https://testing.todorvachev.com/selectors/class-name";
+        string className = "testClass";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.ClassName(className));
+
+        GreenMessage(element.Text);
+
+        driver.Quit();
+    }
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
