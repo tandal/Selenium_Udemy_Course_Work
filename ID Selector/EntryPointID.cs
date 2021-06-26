e4721093cab44809a5e7b900e08e3efc3f987168
tylerandal <tyler.andal@gmail.com>
2021-06-26 07:32:54 -0500

    Add project files.

---
+++ ID Selector/EntryPointID.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPointID
+{
+    static void Main()
+    {
+        string url = "https://testing.todorvachev.com/selectors/id";
+        string photoID = "testImage";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+
+        try
+        {
+            IWebElement element = driver.FindElement(By.Id(photoID));
+            GreenMessage("The PhotoID element is available");
+        }
+        catch {
+            RedMessage("Unable to find the PhotoID Element");
+        }
+
+         driver.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ SetupEnvironment/EntryPoint.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://testing.todorvachev.com/selectors/name");
+
+        IWebElement element = driver.FindElement(By.Name("myName"));
+
+        if (element.Displayed)
+        {
+            GreenMessage("The input text field on the Name page is available.");
+        }
+        else {
+            RedMessage("The input text field on the Name page is not available.");
+        }
+
+        driver.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
